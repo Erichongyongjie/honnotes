559d21649c64928ca5fddc1e3c6b2a51b1c3ea37
Erichongyongjie <88810660+Erichongyongjie@users.noreply.github.com>
2023-04-19 13:15:48 +0800

    Update zh.ts

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -2,7 +2,7 @@ import { navbar } from "vuepress-theme-hope";
 
 export const zhNavbar = navbar([
   "/",
-  { text: "配置", icon: "date", link: "peizhi.md" },
+  { text: "配置", icon: "date", link: "/peizhi.md" },
   { text: "分享", icon: "discover", link: "/demo/" },
   {
     text: "笔记",
@@ -36,10 +36,16 @@ export const zhNavbar = navbar([
       },
     ],
    },
-  { text: "关于", icon: "discover", link: "/about/" },
+  {
+    text: "藏阁",
+    icon: "list",
+    link: "",
+	children: [{ text: "藏词阁", icon: "article", link: "/cang/cangci.md" }, { text: "藏句阁", icon: "article", link: "/cang/cangju.md" }, { text: "藏诗阁", icon: "article", link: "/cang/cangshi.md" }],
+  },
+  { text: "关于", icon: "info", link: "/about/" },
   {
     text: "工具",
-    icon: "note",
+    icon: "config",
     link: "",
 	children: [{ text: "导航", icon: "more", link: "http://dh.hon.ink" }, { text: "早报", icon: "more", link: "http://news.icodeq.com" }, { text: "一文", icon: "more", link: "https://meiriyiwen.com/" }],
   },
